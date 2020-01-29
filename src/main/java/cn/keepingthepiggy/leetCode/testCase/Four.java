73de19e360deaaa044f0555a5ae377247bf53f99
woon9 <woon93@163.com>
2020-01-29 21:41:01 +0800

    leetCode practice Four

--- src/main/java/cn/keepingthepiggy/leetCode/testCase/Four.java
+++ src/main/java/cn/keepingthepiggy/leetCode/testCase/Four.java
@@ -2,6 +2,9 @@ package cn.keepingthepiggy.leetCode.testCase;
 
 import cn.keepingthepiggy.leetCode.Paramz;
 import cn.keepingthepiggy.leetCode.TestCase;
+import sun.font.DelegatingShape;
+
+import java.util.Arrays;
 
 /**
  * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
@@ -32,7 +35,7 @@ public class Four extends TestCase {
 
     @Override
     public void initParam() {
-        params[0].nums = new int[]{1, 3, 5};
+        params[0].nums = new int[]{1, 3, 5, 6, 9, 9, 9, 9};
         params[1].nums = new int[]{2, 4, 6};
     }
 
@@ -42,12 +45,18 @@ public class Four extends TestCase {
         int[] nums2 = params[1].nums;
 
 
-        int[] listMax = nums1.length > nums2.length ? nums1 : nums2;
+        int[] listMax = nums1.length >= nums2.length ? nums1 : nums2;
         int[] listMin = nums1.length < nums2.length ? nums1 : nums2;
+        System.out.println("listMax: " + Arrays.toString(listMax));
+        System.out.println("listMin: " + Arrays.toString(listMin));
 
         int lenMIn = listMin.length;
 
         double middle = ((double) nums1.length + (double) nums2.length) / 2;
+        System.out.println("middle: " + middle);
+        System.out.println("middle * 100 % 100: " + middle * 100 % 100);
+        boolean isOdd = middle * 100 % 100 != 0;
+        System.out.println("isOdd: " + isOdd);
 
         int times = 0;
         int detector = 0;
@@ -63,15 +72,33 @@ public class Four extends TestCase {
             times++;
 
             if (times >= middle) {
-                if (times > middle) {
-                    target = Math.max(listMin[detector], listMax[i]);
+                if (isOdd) {
+                    if (times - middle < 1.00) {
+                        target = listMax[i];
+                    } else {
+                        target = listMin[detector - 1];
+                    }
+                    System.out.println("is Odd");
+                    System.out.println("detector: " + detector);
+                    System.out.println("times: " + times);
+                    System.out.println("target: " + target);
+                    break;
                 } else {
+                    if (times > middle) {
+                        target = (double) (listMin[detector - 1] + listMax[i]) / 2;
+                    } else {
+                        int next = (listMin[detector] <= listMax[i + 1]) ? listMin[detector] : listMax[i + 1];
+                        target = (double) (listMax[i] + next) / 2;
+                    }
 
+                    System.out.println("is Not Odd");
+                    System.out.println("detector: " + detector);
+                    System.out.println("times: " + times);
+                    System.out.println("target: " + target);
+                    break;
                 }
-
-
-                break;
             }
+
         }
 
     }
