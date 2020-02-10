523fc821b96d79bed4f8dfedceedf2ab7cd95840
woon9 <woon93@163.com>
2020-02-10 14:19:25 +0800

    myshow logback

--- src/main/java/cn/keepingthepiggy/PiggyApplication.java
+++ src/main/java/cn/keepingthepiggy/PiggyApplication.java
@@ -17,29 +17,17 @@ import java.util.Map;
 public class PiggyApplication {
 
 	public static void main(String[] args) {
-//		Map aaa = new HashMap();
-//		Map bbb = new HashMap();
-//		bbb.put("bbb","bbb");
-//		aaa.put("aaa",bbb);
-//		bbb.put("bbb","ccc");
-//		aaa.put("aaa","aaa");
-
-
-
-
-		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TestConfig.class);
-//		System.gc();
-		TheName theName = ctx.getBean(TheName.class);
-		theName.getName();
-		System.out.println(theName.getName());
-
-
+//		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(TestConfig.class);
+////		System.gc();
+//		TheName theName = ctx.getBean(TheName.class);
+//		theName.getName();
+//		System.out.println(theName.getName());
 
 		SpringApplication.run(PiggyApplication.class, args);
 
-		HelloWorld hw = ctx.getBean(HelloWorld.class);
-		hw.helloWorld();
-		System.out.println(hw.helloWorld());
+//		HelloWorld hw = ctx.getBean(HelloWorld.class);
+//		hw.helloWorld();
+//		System.out.println(hw.helloWorld());
 
 	}
 
--- src/main/java/cn/keepingthepiggy/letus/HelloWorld.java
+++ src/main/java/cn/keepingthepiggy/letus/HelloWorld.java
@@ -1,16 +1,38 @@
 package cn.keepingthepiggy.letus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/ask")
 public class HelloWorld {
-    @RequestMapping("/")
-    public String helloWorld() {
-        return "Hello, Piggy~";
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("")
+    public String helloAsk() {
+        logger.info("Hello ASK~");
+        return "Hello ASK~";
     }
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        logger.info("Hello World~");
+        return "Hello World~";
+    }
+
+    @RequestMapping("/register")
+    public Map register() {
+        logger.info("register");
+        Map responsible = new HashMap();
+        responsible.put("flag","1");
+        return responsible;
+    }
+
 }
